8fbe655c865f13e21280af0d575b676533425991
Riley O'Shea <roshea@uccs.edu>
2025-06-25 12:40:23 -0600

    Update canvasAPI.py

--- config/canvasAPI.py
+++ config/canvasAPI.py
@@ -1,2 +1,2 @@
 #individual API Token for each user do not copy
-CANVAS_API_TOKEN = "8532~fJTyDYWt3zGGvAuUH8awGeXx3uEAZrE2WFHyMmeKRfuZNew4w7maxZNGrtMXuCY7"
+CANVAS_API_TOKEN = "PASTE API TOKEN HERE"
